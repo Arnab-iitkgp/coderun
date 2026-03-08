28730e1843f3c5865f35b0b614bcf47532f66cdb
Arnab-iitkgp <mrarnab7574@gmail.com>
2026-03-08 15:35:52 +0530

    initial setup

---
+++ prisma/migrations/20260308095116_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Submission" (
+    "id" SERIAL NOT NULL,
+    "problemId" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "language" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "output" TEXT,
+    "error" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Submission_pkey" PRIMARY KEY ("id")
+);
